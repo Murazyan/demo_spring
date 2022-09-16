78f66b6f475d6231d4c1e01ed58a8e5390222b4a
Margarita Murazyan <murazyan.margarita@gmail.com>
2022-09-16 21:31:34 +0400

    Initial commit

---
+++ src/main/java/com/example/demo_spring/Controller.java
@@ -0,0 +1,16 @@
+package com.example.demo_spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class Controller {
+
+
+    @GetMapping
+    public String test(){
+        return "Hello world";
+    }
+}
---
+++ src/main/java/com/example/demo_spring/DemoSpringApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        SpringApplication.run(DemoSpringApplication.class, args);
+    }
+
+}
